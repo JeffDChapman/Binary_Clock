7deea28a379db6dc99abc4c7ad4c53147363c637
JeffDChapman <jeffdchapman@hotmail.com>
2016-11-15 14:39:23 -0800

    Binary Clock
    Color-configurable Binary Clock in c#

---
+++ BinaryClock.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmbinaryclock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmbinaryclock));
+            this.Timerbackdetail = new System.Windows.Forms.Timer(this.components);
+            this.label01 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label021 = new System.Windows.Forms.Label();
+            this.label022 = new System.Windows.Forms.Label();
+            this.label023 = new System.Windows.Forms.Label();
+            this.label024 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.label33 = new System.Windows.Forms.Label();
+            this.label031 = new System.Windows.Forms.Label();
+            this.label032 = new System.Windows.Forms.Label();
+            this.label033 = new System.Windows.Forms.Label();
+            this.label034 = new System.Windows.Forms.Label();
+            this.Timercolors = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.themeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.trcpyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbarOpacity = new System.Windows.Forms.TrackBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarOpacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timerbackdetail
+            // 
+            this.Timerbackdetail.Enabled = true;
+            this.Timerbackdetail.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label01
+            // 
+            this.label01.Location = new System.Drawing.Point(11, 131);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(26, 26);
+            this.label01.TabIndex = 3;
+            // 
+            // label02
+            // 
+            this.label02.Location = new System.Drawing.Point(11, 98);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(26, 26);
+            this.label02.TabIndex = 4;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(43, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 26);
+            this.label11.TabIndex = 5;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(43, 98);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 26);
+            this.label12.TabIndex = 6;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(43, 66);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 26);
+            this.label13.TabIndex = 7;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(43, 34);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(26, 26);
+            this.label14.TabIndex = 8;
+            // 
+            // label21
+            // 
+            this.label21.Location = new System.Drawing.Point(89, 131);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(26, 26);
+            this.label21.TabIndex = 9;
+            // 
+            // label22
+            // 
+            this.label22.Location = new System.Drawing.Point(89, 98);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(26, 26);
+            this.label22.TabIndex = 10;
+            // 
+            // label23
+            // 
+            this.label23.Location = new System.Drawing.Point(89, 66);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(26, 26);
+            this.label23.TabIndex = 11;
+            // 
+            // label021
+            // 
+            this.label021.Location = new System.Drawing.Point(121, 131);
+            this.label021.Name = "label021";
+            this.label021.Size = new System.Drawing.Size(26, 26);
+            this.label021.TabIndex = 12;
+            // 
+            // label022
+            // 
+            this.label022.Location = new System.Drawing.Point(121, 98);
+            this.label022.Name = "label022";
+            this.label022.Size = new System.Drawing.Size(26, 26);
+            this.label022.TabIndex = 13;
+            // 
+            // label023
+            // 
+            this.label023.Location = new System.Drawing.Point(121, 66);
+            this.label023.Name = "label023";
+            this.label023.Size = new System.Drawing.Size(26, 26);
+            this.label023.TabIndex = 14;
+            // 
+            // label024
+            // 
+            this.label024.Location = new System.Drawing.Point(121, 34);
+            this.label024.Name = "label024";
+            this.label024.Size = new System.Drawing.Size(26, 26);
+            this.label024.TabIndex = 15;
+            this.label024.Click += new System.EventHandler(this.label024_Click);
+            // 
+            // label31
+            // 
+            this.label31.Location = new System.Drawing.Point(164, 131);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(26, 26);
+            this.label31.TabIndex = 16;
+            // 
+            // label32
+            // 
+            this.label32.Location = new System.Drawing.Point(164, 98);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(26, 26);
+            this.label32.TabIndex = 17;
+            // 
+            // label33
+            // 
+            this.label33.Location = new System.Drawing.Point(164, 66);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(26, 26);
+            this.label33.TabIndex = 18;
+            // 
+            // label031
+            // 
+            this.label031.Location = new System.Drawing.Point(196, 131);
+            this.label031.Name = "label031";
+            this.label031.Size = new System.Drawing.Size(26, 26);
+            this.label031.TabIndex = 19;
+            // 
+            // label032
+            // 
+            this.label032.Location = new System.Drawing.Point(196, 98);
+            this.label032.Name = "label032";
+            this.label032.Size = new System.Drawing.Size(26, 26);
+            this.label032.TabIndex = 20;
+            // 
+            // label033
+            // 
+            this.label033.Location = new System.Drawing.Point(196, 66);
+            this.label033.Name = "label033";
+            this.label033.Size = new System.Drawing.Size(26, 26);
+            this.label033.TabIndex = 21;
+            // 
+            // label034
+            // 
+            this.label034.Location = new System.Drawing.Point(196, 34);
+            this.label034.Name = "label034";
+            this.label034.Size = new System.Drawing.Size(26, 26);
+            this.label034.TabIndex = 22;
+            // 
+            // Timercolors
+            // 
+            this.Timercolors.Tick += new System.EventHandler(this.Timercolors_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Gray;
+            this.menuStrip1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.themeToolStripMenuItem,
+            this.blueToolStripMenuItem1,
+            this.greenToolStripMenuItem1,
+            this.trcpyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(235, 24);
+            this.menuStrip1.TabIndex = 24;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // themeToolStripMenuItem
+            // 
+            this.themeToolStripMenuItem.Name = "themeToolStripMenuItem";
+            this.themeToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.themeToolStripMenuItem.Text = "On";
+            this.themeToolStripMenuItem.Click += new System.EventHandler(this.themeToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem1
+            // 
+            this.blueToolStripMenuItem1.Name = "blueToolStripMenuItem1";
+            this.blueToolStripMenuItem1.Size = new System.Drawing.Size(36, 20);
+            this.blueToolStripMenuItem1.Text = "Off";
+            this.blueToolStripMenuItem1.Click += new System.EventHandler(this.blueToolStripMenuItem1_Click);
+            // 
+            // greenToolStripMenuItem1
+            // 
+            this.greenToolStripMenuItem1.Name = "greenToolStripMenuItem1";
+            this.greenToolStripMenuItem1.Size = new System.Drawing.Size(39, 20);
+            this.greenToolStripMenuItem1.Text = "Bkd";
+            this.greenToolStripMenuItem1.Click += new System.EventHandler(this.greenToolStripMenuItem1_Click);
+            // 
+            // trcpyToolStripMenuItem
+            // 
+            this.trcpyToolStripMenuItem.Name = "trcpyToolStripMenuItem";
+            this.trcpyToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.trcpyToolStripMenuItem.Text = "Trcpy";
+            this.trcpyToolStripMenuItem.Click += new System.EventHandler(this.trcpyToolStripMenuItem_Click);
+            // 
+            // tbarOpacity
+            // 
+            this.tbarOpacity.LargeChange = 10;
+            this.tbarOpacity.Location = new System.Drawing.Point(9, 34);
+            this.tbarOpacity.Maximum = 100;
+            this.tbarOpacity.Minimum = 10;
+            this.tbarOpacity.Name = "tbarOpacity";
+            this.tbarOpacity.Size = new System.Drawing.Size(212, 45);
+            this.tbarOpacity.SmallChange = 10;
+            this.tbarOpacity.TabIndex = 25;
+            this.tbarOpacity.TickFrequency = 10;
+            this.tbarOpacity.Value = 100;
+            this.tbarOpacity.Visible = false;
+            this.tbarOpacity.Scroll += new System.EventHandler(this.tbarOpacity_Scroll);
+            // 
+            // frmbinaryclock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(235, 169);
+            this.Controls.Add(this.tbarOpacity);
+            this.Controls.Add(this.label034);
+            this.Controls.Add(this.label033);
+            this.Controls.Add(this.label032);
+            this.Controls.Add(this.label031);
+            this.Controls.Add(this.label33);
+            this.Controls.Add(this.label32);
+            this.Controls.Add(this.label31);
+            this.Controls.Add(this.label024);
+            this.Controls.Add(this.label023);
+            this.Controls.Add(this.label022);
+            this.Controls.Add(this.label021);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.label01);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmbinaryclock";
+            this.Text = "BitClock";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarOpacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timerbackdetail;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label021;
+        private System.Windows.Forms.Label label022;
+        private System.Windows.Forms.Label label023;
+        private System.Windows.Forms.Label label024;
+        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.Label label32;
+        private System.Windows.Forms.Label label33;
+        private System.Windows.Forms.Label label031;
+        private System.Windows.Forms.Label label032;
+        private System.Windows.Forms.Label label033;
+        private System.Windows.Forms.Label label034;
+        private System.Windows.Forms.Timer Timercolors;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem themeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem trcpyToolStripMenuItem;
+        private System.Windows.Forms.TrackBar tbarOpacity;
+    }
+}
+
---
+++ BinaryClock.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmbinaryclock : Form
+    {
+        System.Drawing.Color onColor;
+        System.Drawing.Color offColor;
+        String strseconds;
+        String strminutes;
+        String strhours;
+        int inthoursbig;
+        int inthourssmall;
+        int intminutesbig;
+        int intminutessmall;
+        int intsecondsbig;
+        int intsecondssmall;
+        public void Colorful(int time, int numclolom)
+        {
+            switch (numclolom)
+            {
+                case 1:
+                    color1columnDots(time, label01, label02, label02, label02);
+                    break;
+                case 2:
+                    color1columnDots(time, label11, label12, label13, label14);
+                    break;
+                case 3:
+                    color1columnDots(time, label21, label22, label23, null);
+                    break;
+                case 4:
+                    color1columnDots(time, label021, label022, label023, label024);
+                    break;
+                case 5:
+                    color1columnDots(time, label31, label32, label33, null);
+                    break;
+                case 6:
+                    color1columnDots(time, label031, label032, label033, label034);
+                    break;
+            }
+        }
+
+        private void color1columnDots(int time, Label lab1, Label lab2, Label lab3, Label lab4)
+        {
+            string resultBin = Convert.ToString(time, 2).PadLeft(4,'0');
+            if (resultBin.Substring(3,1) == "0")
+                { lab1.BackColor = offColor; }
+            else 
+                { lab1.BackColor = onColor; }
+
+            if (resultBin.Substring(2, 1) == "0")
+                { lab2.BackColor = offColor; }
+            else
+                { lab2.BackColor = onColor; }
+
+            if (resultBin.Substring(1, 1) == "0")
+                { lab3.BackColor = offColor; }
+            else
+                { lab3.BackColor = onColor; }
+
+            try
+            {
+            if (time < 8)
+                { lab4.BackColor = offColor; }
+            else
+                { lab4.BackColor = onColor; }
+            }
+            catch{}
+
+        }
+
+        public frmbinaryclock()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            onColor = Color.Blue;
+            offColor = Color.DarkSlateGray;
+
+            Timerbackdetail.Enabled = true;
+            Timercolors.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void lblseconds_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Timercolors_Tick(object sender, EventArgs e)
+        {
+
+
+            strminutes = string.Format("{0:mm}", DateTime.Now);
+            strhours = string.Format("{0:HH}", DateTime.Now);
+            strseconds = string.Format("{0:ss}", DateTime.Now);
+            inthoursbig = Convert.ToInt32(Convert.ToString((strhours[0])));
+            inthourssmall = Convert.ToInt32(Convert.ToString((strhours[1])));
+            intminutesbig = Convert.ToInt32(Convert.ToString((strminutes[0])));
+            intminutessmall = Convert.ToInt32(Convert.ToString((strminutes[1])));
+            intsecondsbig = Convert.ToInt32(Convert.ToString(strseconds[0]));
+            intsecondssmall = Convert.ToInt32((Convert.ToString(strseconds[1])));
+
+            Colorful(inthoursbig, 1);
+            Colorful(inthourssmall, 2);
+            Colorful(intminutesbig, 3);
+            Colorful(intminutessmall, 4);
+            Colorful(intsecondsbig, 5);
+            Colorful(intsecondssmall, 6);
+        }
+
+        private void lblseconds_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timerconfig_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label024_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void themeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.Color = onColor;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                onColor = MyDialog.Color;
+
+        }
+
+        private void blueToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.Color = offColor;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                offColor = MyDialog.Color;
+        }
+
+        private void yellowToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            onColor = Color.Yellow;
+        }
+
+        private void greenToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.Color = this.BackColor;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                this.BackColor = MyDialog.Color;
+
+        }
+
+        private void purpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            onColor = Color.Purple;
+        }
+
+        private void trcpyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.tbarOpacity.Visible == true)
+                { this.tbarOpacity.Visible = false; return; }
+            this.tbarOpacity.Visible = true;
+        }
+
+        private void tbarOpacity_Scroll(object sender, EventArgs e)
+        {
+            double myOpac = this.tbarOpacity.Value;
+            this.Opacity = myOpac / 100;
+            this.Refresh();
+        }
+
+
+    }
+
+}
